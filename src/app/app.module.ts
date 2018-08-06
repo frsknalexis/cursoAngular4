aee1b3965f486d301490092c5168eee3d24a9122
frsknalexis <alexisgutierrezf.1997@gmail.com>
2018-08-06 02:33:12 -0500

    Implementacion de Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Componentes
 import { AppComponent } from './app.component';
+import { TiendaComponent } from './components/tienda/tienda.component';
+import { ParquesComponent } from './components/parques/parques.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TiendaComponent,
+    ParquesComponent
   ],
   imports: [
     BrowserModule
